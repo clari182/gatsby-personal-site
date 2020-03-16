f4d781dadf4e1da34b0ed51cc640d368942bc4f6
Clara Youdale <clara@thebotmakers.com>
2020-03-16 14:50:40 -0300

    Update index.js

--- src/data/menu/index.js
+++ src/data/menu/index.js
@@ -5,11 +5,6 @@ const Menudata = [
         href: '#home'
     },
     {
-        id: 2,
-        title: 'About',
-        href: '#about'
-    },
-    {
         id: 3,
         title: 'Skills',
         href: '#skills'
