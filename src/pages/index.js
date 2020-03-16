d57aaadd30bb97ef45f0c44d4e40782cd03c0dd2
Clara Youdale <clara@thebotmakers.com>
2020-03-16 16:10:37 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,22 @@ import BusinessTheme from '../components/businesstheme';
 import 'antd/dist/antd.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import Helmet from "react-helmet";
 
 const IndexPage = () => (
-  <div className="main-wrapper">
+    <>
+
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Clara Youdale</title>
+            <link rel="canonical" href="https://clarayoudale.com" />
+        </Helmet>
+        <div className="main-wrapper">
             <BusinessTheme
                 ThemeClass="demo1"
             />
         </div>
+    </>
 )
 
 export default IndexPage
