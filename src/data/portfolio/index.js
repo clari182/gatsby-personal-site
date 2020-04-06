0d7cd3abbef1b7594db8e27019e1916078da018d
Clara Youdale <clara@thebotmakers.com>
2020-04-06 09:29:30 -0300

    add yo ayudo

--- src/data/portfolio/index.js
+++ src/data/portfolio/index.js
@@ -3,31 +3,39 @@ import cosmichype from '../../assets/images/portfolio/cosmichype.png';
 import botsfactory from '../../assets/images/portfolio/botsfactory.png';
 import astrolobot from '../../assets/images/portfolio/astrolobot.png';
 import whatsapp from '../../assets/images/portfolio/whatsapp.png';
+import yoayudo from '../../assets/images/portfolio/yoayudo.JPG';
 
 const PortfolioData = [
   {
     "id": 1,
+    "img": yoayudo,
+    "subtitle": "Yo Ayudo UY",
+    "subcontent": "Non-profit project to help Uruguay's local businesses open during COVID-19's crisis.",
+    "url": "https://yoayudo.uy"
+  }, 
+  {
+    "id": 2,
     "img": botsfactory,
     "subtitle": "Botsfactory",
     "subcontent": "Botsfactory is a Chatbot Building platform to create and deploy Messenger Chatbots without coding.",
     "url": "https://botsfactory.io"
   },
   {
-    "id": 2,
+    "id": 3,
     "img": cosmichype,
     "subtitle": "Cosmic Hype",
     "subcontent": "Cosmic Hype is a trending news website.",
     "url": "https://cosmichype.com"
   },
   {
-    "id": 3,
+    "id": 4,
     "img": astrolobot,
     "subtitle": "Astrolobot Chatbot",
     "subcontent": "Astrology chatbot",
     "url": "https://m.me/cosmichype"
   },
   {
-    "id": 4,
+    "id": 5,
     "img": whatsapp,
     "subtitle": "Whatsapp Chatbot",
     "subcontent": "Business Whatsapp Chatbot for Botsfactory",
